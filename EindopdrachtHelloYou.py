5e85a5b480ec8638ac56199ef7a69987ada2d3ac
BenteKavsek <35726@ma-web.nl>
2022-10-08 23:54:47 +0200

    Begin van verhaaltjes en definitief idee

--- EindopdrachtHelloYou.py
+++ EindopdrachtHelloYou.py
@@ -1,24 +1,114 @@
+#functions met stukjes verhaaltjes
 
-from re import A
+def function1():
+    print("Welkom bij mijn eindopdracht van periode 1 Hello You, ik heb een textbased applicatie gemaakt die je een verhaal zal laten beleven.")
+    print("Hierin ben jij de hoofdpersoon, en kan je zelf keuzes maken om te zien hoe het verhaal verder gaat en uiteindelijk afloopt.")
 
-
-def functie1():
-    print("1.	Welkom bij mijn eindopdracht van periode 1 Hello You, ik heb een textbased applicatie gemaakt die je een verhaal zal laten beleven.")
-    print("Hierin ben jij de hoofdpersoon, en kan je zelf keuzes maken om te zien hoe het verhaal verdergaat en uiteindelijk afloopt.")
-    antwoord1 = input("kleine stukjes uit het verhaal heb ik gebaseerd op mijzelf, Wil je beginnen?\n A, ja! \n B, nee")
-   
 
 def function2():
-    antwoord2 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+    print("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, (Let op! het is hoofdletter gevoelig!) \n Have Fun!")
 
-    
 
 def function3():
-    antwoord2 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+    print("Jammer, tot n andere keer!")
 
- 
 
-#if antwoord 1 == A
-#    fuction 12()
-#elif atwoord1 == B()
-#    FUNCTION 3()
+def function4():
+    print("het werkt tot nu toe")
+
+
+def function5():
+    print("faillll")
+
+
+def function7():
+    antwoord7 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function8():
+    antwoord8 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function9():
+    antwoord9 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function10():
+    antwoord10 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function11():
+    antwoord11 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function12():
+    antwoord12 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function13():
+    antwoord13 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function14():
+    antwoord14 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function15():
+    antwoord15 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function16():
+    antwoord16 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function17():
+    antwoord17 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function18():
+    antwoord18 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function19():
+    antwoord19 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function20():
+    antwoord20 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+
+def function21():
+    antwoord21 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function1()
+antwoord1 = input("kleine stukjes uit het verhaal heb ik gebaseerd op mijzelf, Wil je beginnen?\n A, ja! \n B, nee\n")
+
+if antwoord1 == "A":
+    function2()
+    antwoord2 = input("Om te Beginnen typ: START")
+
+elif antwoord1 == "B":
+    function3()
+
+
+if antwoord2 == "START":
+    function4()
+
+while antwoord2 != "START":
+    print("\n Probeer nog een keer, \n")
+    function2()
+    antwoord2 = input("Om te Beginnen typ: START")
+    if antwoord2 == "START":
+        function4()
+
+
+
+#if antwoord... == "...":
+#    function...()
+#    antwoord... = input("...")
+#
+#elif antwoord... == "...":
+#    function...()
+#    antwoord... = input("...")
