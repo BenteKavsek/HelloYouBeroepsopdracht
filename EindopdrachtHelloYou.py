499362b4605543b71702266c56b4d51a65699cd8
BenteKavsek <35726@ma-web.nl>
2022-10-08 22:58:20 +0200

    eerste ideeen hoe ik het wil doen

---
+++ EindopdrachtHelloYou.py
@@ -0,0 +1,24 @@
+
+from re import A
+
+
+def functie1():
+    print("1.	Welkom bij mijn eindopdracht van periode 1 Hello You, ik heb een textbased applicatie gemaakt die je een verhaal zal laten beleven.")
+    print("Hierin ben jij de hoofdpersoon, en kan je zelf keuzes maken om te zien hoe het verhaal verdergaat en uiteindelijk afloopt.")
+    antwoord1 = input("kleine stukjes uit het verhaal heb ik gebaseerd op mijzelf, Wil je beginnen?\n A, ja! \n B, nee")
+   
+
+def function2():
+    antwoord2 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+    
+
+def function3():
+    antwoord2 = input("Leuk dat je wilt beginnen , om keuzes te maken typ je alleen de letter die ervoor staat, dus bijvoorbeeld a of b, \n Have Fun!")
+
+ 
+
+#if antwoord 1 == A
+#    fuction 12()
+#elif atwoord1 == B()
+#    FUNCTION 3()
