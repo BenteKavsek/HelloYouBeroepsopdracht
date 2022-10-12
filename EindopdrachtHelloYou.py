fa133acb8df02197c3a777fd574d9c4a0f0de4aa
BenteKavsek <35726@ma-web.nl>
2022-10-12 10:55:01 +0200

    einde  les bo 12-10

--- EindopdrachtHelloYou.py
+++ EindopdrachtHelloYou.py
@@ -1,4 +1,7 @@
 import random
+from time import sleep
+import keyboard
+
 #functions met stukjes verhaaltjes
 
 def function1():
@@ -22,29 +25,38 @@ def function4():
 def function5():
     print("Okee lekker zelfverzekerd, nu hopen dat het goedkomt!")
 
+
 def function6():
-    print("")#nog ff leren
+    print("Jammer, de toets weegt zwaarder mee dan je dacht, je bent niet geslaagd en moet het jaar over doen, je kan dus niet komend jaar naar het buitenland. Volgende keer beter!")#nog ff leren
 
 
 def function7():
-    print("Jammer, de toets weegt zwaarder mee dan je dacht, je bent niet geslaagd en moet het jaar over doen, je kan dus niet komend jaar naar het buitenland. Volgende keer beter!")
-
-
-def function8():
     print("Luckyyy! Je stond er al goed voor, het was dus niet zo erg dat je niet alles wist. je bent geslaagd en krijgt een uitnodiging binnen voor je diploma uitreiking,")
     print("eenmaal daar die je veel docenten weer en praat je bij met je klasgenoten, je verteld ze over de reis naar het buitenland die je wilt gaan maken, spannend!")
 
 
+def function8():
+    #iets met if en n functie voor eerst print
+    print("Slim! Je slaagt en hebt je vakantie dubbel verdient! ")
+    print("Je krijgt een uitnodiging binnen voor je diploma uitreiking, eenmaal daar die je veel docenten weer en praat je bij met je klasgenoten,\nje verteld ze over de reis naar het buitenland die je wilt gaan maken, spannend! ")
+
 def function9():
-    print("...")
+    print("Je gaat een jaar naar het buitenland, gaaf! Hoe wil je gaan? Met het vliegtuig of rijden")
 
 
 def function10():
-    print("...")
+    print("Prima! Je vliegt overmorgen om 6:15 naar Palermo in Sicilië")
+    print("hier ben je al een keer met je schoonfamilie geweest en het was ongelofelijk mooi, je gaat vanavond afscheid nemen")
+    print("ze willen wel graag op bezoek komen en helpen je alvast met inpakken, zo kan je rustig je vakantie in gaan.\n")
+
+def function102():
+    print("Die ochtend zelf ga je met het OV naar Schiphol, je appt iedereen je vluchtnummer en gegevens en zoekt je gate om in te checken,")
+    print("even later, nog voordat je bij security bent lijkt het alsof je iemand herkent, je kan alleen niet plaatsen wie het is of waarvan je hem kent, wat doe je? ")
 
 
 def function11():
-    print("...")
+    print("Hahaha, in al je enthousiasme ben je vergeten dat je geen rijbewijs hebt")
+    print("je kan nog een last minute vliegticket proberen te scoren dan ben je er snel, of je kan gaan liften, misschien leer je wel allem aal leuke mensen kennen!")
 
 
 def function12():
@@ -52,7 +64,7 @@ def function12():
 
 
 def function13():
-    print("...")
+    print("Je ouders kunne je wegbrengen en daarna stap je bij allerlei mensen in, sommige stil, sommige heel gezellig, ziet iemand die je lijkt te herkennen, wat doe je?")
 
 
 def function14():
@@ -102,16 +114,30 @@ elif antwoord1 == "B":
 
 if antwoord4 == "Ä":
     function5()
+    a = (random.randint(1, 100))
+    if a >50:
+        function7()
+    elif a <=50:
+        function8()
+        sleep(2)
+        function9()
+        antwoord9 = input("\nA, het vliegtuig\nB,rijden")
 
 elif antwoord4 == "B":
-    function6()
-
-a = (random.randint(1, 100))
-if a >50:
-    function7()
-elif a <=50:
     function8()
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
+    sleep(2)
+    function9()
+    antwoord9 = input("\nA, het vliegtuig\nB,rijden")
+
+if antwoord9 == "A":
+    function10()
+    sleep(2)
+    function102()
+
+elif antwoord9 == "B":
+    function11()
+    antwoord11 = input("A, je gaat alsnog vliegen want je kan niet wachten om er te zijn!\nB,Liften! nieuwe mensen leren kennen kan nooit kwaad!")
+#--------------------------------------------------------------------------------------------------------------------
 
 #if antwoord... == "...":
 #    function...()
